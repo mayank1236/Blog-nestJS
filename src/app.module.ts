7b48d38eadb81f22539886676a2f4953e9468fcf
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-01-14 00:01:32 +0530

    user features module, userController & userService, created userEntity & userInterface and postman request

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { UserEntity } from './user/models/user.entity';
 
 @Module({
   imports: [
@@ -17,9 +19,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: 'blog',
-      entities: [],
+      entities: [UserEntity],
       synchronize: true,
-    })
+    }),
+
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
